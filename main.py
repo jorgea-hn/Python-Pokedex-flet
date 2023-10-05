6e4be9ff80087c7b8b3e18267056b3a4658e22e0
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2023-10-05 17:08:23 -0500

    Center content

--- main.py
+++ main.py
@@ -10,19 +10,37 @@ async def main(page: ft.page):
 
     boton_azul = ft.Stack([
             ft.Container(width=80,height=80,bgcolor=ft.colors.WHITE ,border_radius=50),
-            ft.Container(width=70,height=70,left=4, top=3, bgcolor=ft.colors.BLUE, border_radius=50),
+            ft.Container(width=70,height=70,left=5, top=5, bgcolor=ft.colors.BLUE, border_radius=50),
             
     ])
 
     items_superior=[
-        ft.Container(boton_azul, width=80, height=80, border=ft.border.all()),
-        ft.Container(width=40, height=40, border=ft.border.all(), bgcolor=ft.colors.RED_200, border_radius=50),
-        ft.Container(width=40, height=40, border=ft.border.all(), bgcolor=ft.colors.YELLOW, border_radius=50),
-        ft.Container(width=40, height=40, border=ft.border.all(), bgcolor=ft.colors.GREEN, border_radius=50)
+        ft.Container(boton_azul, width=80, height=80),
+        ft.Container(width=40, height=40, bgcolor=ft.colors.RED_200, border_radius=50),
+        ft.Container(width=40, height=40, bgcolor=ft.colors.YELLOW, border_radius=50),
+        ft.Container(width=40, height=40, bgcolor=ft.colors.GREEN, border_radius=50)
     ]
 
-    superior = ft.Container(content=ft.Row(items_superior), width=600, height=80,margin=ft.margin.only(top=40), border=ft.border.all())
-    centro = ft.Container(width=600, height=400,margin=ft.margin.only(top=40), border=ft.border.all())
+
+    stack_central = ft.Stack([
+        ft.Container(width=600, height=400, bgcolor=ft.colors.WHITE),
+        ft.Container(width=550, height=350, bgcolor=ft.colors.BLACK, top=25, left=25),
+        ft.Image(
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png",
+            scale=10,
+            width=50,
+            height=50,
+            top=350/2,
+            right=550/2
+
+        )
+    ])
+
+
+
+
+    superior = ft.Container(content=ft.Row(items_superior), width=600, height=80,margin=ft.margin.only(top=40))
+    centro = ft.Container(content=stack_central, width=600, height=400,margin=ft.margin.only(top=40), alignment=ft.alignment.center)
     inferior = ft.Container(width=600, height=400,margin=ft.margin.only(top=40), border=ft.border.all())
 
     col = ft.Column(spacing=0, controls=[
